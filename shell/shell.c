ba909326e664d95d39a853a80cb033d4e636bc70
Christian Evans <cade9522@gmail.com>
2017-12-06 12:03:38 -0500

    Reflect new shell commands

--- shell/shell.c
+++ shell/shell.c
@@ -1,386 +1,232 @@
 /**
  * @file     shell.c
+ * @provides shell
  *
+ * $Id: shell.c 236 2007-07-12 23:21:31Z brylow $
  */
-/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+/* Embedded XINU, Copyright (C) 2007.  All rights reserved. */
 
-#include <stddef.h>
-#include <ctype.h>
-#include <interrupt.h>
+#include <kernel.h>
 #include <shell.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <tty.h>
-#include <thread.h>
-#include <nvram.h>
-#include <conf.h>
-#include <framebuffer.h>
+#include <proc.h>
 
-const struct centry commandtab[] = {
-#if NETHER
-    {"arp", FALSE, xsh_arp},
-#endif
-    {"clear", TRUE, xsh_clear},
-    {"date", FALSE, xsh_date},
-#if USE_TLB
-    {"dumptlb", FALSE, xsh_dumptlb},
-#endif
-#if NETHER
-    {"ethstat", FALSE, xsh_ethstat},
-#endif
-    {"exit", TRUE, xsh_exit},
-#if NFLASH
-    {"flashstat", FALSE, xsh_flashstat},
-#endif
-#ifdef GPIO_BASE
-    {"gpiostat", FALSE, xsh_gpiostat},
-#endif
-    {"help", FALSE, xsh_help},
-#if defined(ETH0) || defined(_XINU_PLATFORM_ARM_RPI_)
-    {"kexec", FALSE, xsh_kexec},
-#endif
-    {"kill", TRUE, xsh_kill},
-#ifdef GPIO_BASE
-    {"led", FALSE, xsh_led},
-#endif
-    {"memstat", FALSE, xsh_memstat},
-    {"memdump", FALSE, xsh_memdump},
-#if NETHER
-    {"nc", FALSE, xsh_nc},
-    {"netdown", FALSE, xsh_netdown},
-#if NETEMU
-    {"netemu", FALSE, xsh_netemu},
-#endif
-    {"netstat", FALSE, xsh_netstat},
-    {"netup", FALSE, xsh_netup},
-#endif
-#if NVRAM
-    {"nvram", FALSE, xsh_nvram},
-#endif
-    {"ps", FALSE, xsh_ps},
-#if NETHER
-    {"ping", FALSE, xsh_ping},
-    {"pktgen", FALSE, xsh_pktgen},
-    {"rdate", FALSE, xsh_rdate},
-#endif
-    {"reset", FALSE, xsh_reset},
-#if NETHER
-    {"route", FALSE, xsh_route},
-#endif
-    {"sleep", TRUE, xsh_sleep},
-#if NETHER
-    {"snoop", FALSE, xsh_snoop},
-#endif
-#if USE_TAR
-    {"tar", FALSE, xsh_tar},
-#endif
-#if NETHER
-    {"tcpstat", FALSE, xsh_tcpstat},
-    {"telnet", FALSE, xsh_telnet},
-    {"telnetserver", FALSE, xsh_telnetserver},
-#endif
-    {"test", FALSE, xsh_test},
-#if HAVE_TESTSUITE
-    {"testsuite", TRUE, xsh_testsuite},
-#endif
-#if NETHER
-    {"timeserver", FALSE, xsh_timeserver},
-#endif
-#if FRAMEBUF
-    {"turtle", FALSE, xsh_turtle},
-#endif
-#if NUART
-    {"uartstat", FALSE, xsh_uartstat},
-#endif
-#ifdef WITH_USB
-    {"usbinfo", FALSE, xsh_usbinfo},
-#endif
-#if USE_TLB
-    {"user", FALSE, xsh_user},
-#endif
-#if NETHER
-    {"udpstat", FALSE, xsh_udpstat},
-    {"vlanstat", FALSE, xsh_vlanstat},
-    {"voip", FALSE, xsh_voip},
-    {"xweb", FALSE, xsh_xweb},
-#endif
-    {"?", FALSE, xsh_help}
+struct centry commandtab[] = 
+{
+	{ "exit",      TRUE,  xsh_exit     },
+	{ "kill",      TRUE,  xsh_kill     },
+	{ "sleep",     TRUE,  xsh_sleep    },
+        { "chat",      FALSE, xsh_chat     },
+	{ "gpiostat",  FALSE, xsh_gpiostat },
+	{ "help",      FALSE, xsh_help     },
+	{ "led",       FALSE, xsh_led      },
+	{ "memstat",   FALSE, xsh_memstat  },
+	{ "mutex",     FALSE, xsh_mutex    },
+        { "palindrome", FALSE, xsh_palindrome },
+	{ "ps",        FALSE, xsh_ps       },
+	{ "reset",     FALSE, xsh_reset    },
+        { "schedule",  FALSE, xsh_schedule },
+	{ "semaphore", FALSE, xsh_semaphore},
+	{ "uartstat",  FALSE, xsh_uartstat },
+	{ "?",         FALSE, xsh_help     },
+	{ "testsuite", TRUE,  xsh_testsuite },
+	{ "test",      FALSE, xsh_test     }
 };
 
-ulong ncommand = sizeof(commandtab) / sizeof(struct centry);
-extern ulong foreground;
+ulong ncommand = sizeof( commandtab ) / sizeof( struct centry );
 
 /**
- * @ingroup shell
- *
- * The Xinu shell.  Provides an interface to execute commands.
+ * The XINU shell.  Provides an interface to execute commands.
  * @param descrp descriptor of device on which the shell is open
  * @return OK for successful exit, SYSERR for unrecoverable error
  */
-thread shell(int indescrp, int outdescrp, int errdescrp)
+process shell(ushort descrp)
 {
-    char buf[SHELL_BUFLEN];     /* line input buffer        */
-    short buflen;               /* length of line input     */
-    char tokbuf[SHELL_BUFLEN + SHELL_MAXTOK];   /* token value buffer       */
-    short ntok;                 /* number of tokens         */
-    char *tok[SHELL_MAXTOK];    /* pointers to token values */
-    char *outname;              /* name of output file      */
-    char *inname;               /* name of input file       */
-    bool background;            /* is background proccess?  */
-    syscall child;              /* pid of child thread      */
-    ushort i, j;                /* temp variables           */
-    irqmask im;                 /* interrupt mask state     */
-    char *hostptr = NULL;       /* pointer to hostname      */
+	char    buf[SHELL_BUFLEN];                 /* line input buffer        */
+	ushort  buflen;                            /* length of line input     */
+	char    tokbuf[SHELL_BUFLEN+SHELL_MAXTOK]; /* token value buffer       */
+	short   ntok;                              /* number of tokens         */
+	char    *tok[SHELL_MAXTOK];                /* pointers to token values */
+	char    *outname;                          /* name of output file      */
+	char    *inname;                           /* name of input file       */
+	bool    background;                        /* is background proccess?  */
+	ushort  stdin;                             /* stdin  device num        */
+	ushort  stdout;                            /* stdout device num        */
+	ushort  stderr;                            /* stderr device num        */
+	syscall child;                             /* pid of child process     */
+	ushort  i, j;                              /* temp variables           */
 
-    /* Setup buffer for string for nvramGet call for hostname */
-#if defined(ETH0) && NVRAM
-    char hostnm[NET_HOSTNM_MAXLEN + 1]; /* hostname of backend      */
-    if (!isbaddev(ETH0))
-    {
-        size_t hostname_strsz;          /* nvram hostname name size */
+	/* Print shell banner */
+	fprintf(descrp, SHELL_BANNER);
+	/* Print shell welcome message */
+	fprintf(descrp, SHELL_START);
 
-        bzero(hostnm, NET_HOSTNM_MAXLEN + 1);
+	/* Continually receive and handle commands */
+	while (TRUE)
+	{
+		/* Display prompt */
+		fprintf(descrp, SHELL_PROMPT);
 
-        /* Determine the hostname of the main network device */
-        hostname_strsz = strlen(NET_HOSTNAME);
-        hostname_strsz += 1;
-        hostname_strsz += DEVMAXNAME;
-        hostname_strsz += 1;
-        char nvramget_hostname_str[hostname_strsz];
-        sprintf(nvramget_hostname_str, "%s_%s", devtab[ETH0].name,
-                NET_HOSTNAME);
+		/* Enable echo for input and receive input */
+		control(descrp, TTY_IOC_SETIFLAG, TTY_IFLAG_ECHO, NULL);
+		buflen = read(descrp, buf, SHELL_BUFLEN - 1);
 
-        /* Acquire the backend's hostname */
-        hostptr = nvramGet(nvramget_hostname_str);
-        if (hostptr != NULL)
-        {
-            strncpy(hostnm, hostptr, NET_HOSTNM_MAXLEN);
-            hostptr = hostnm;
-        }
-    }
-#endif
+		/* Check for CBREAK, clear vars and loop */
+		if ( control(descrp, TTY_IOC_CBREAK, NULL, NULL) )
+		{
+			buflen = 0;
+			continue;
+		}
 
-    /* Set command devices for input, output, and error */
-    stdin = indescrp;
-    stdout = outdescrp;
-    stderr = errdescrp;
+		/* Check for EOF and exit gracefully if seen */
+		if ( control(descrp, TTY_IOC_EOF, NULL, NULL) ) { break; }
 
-    /* Print shell banner to framebuffer, if exists */
-#if defined(FRAMEBUF)
-    if (indescrp == FRAMEBUF)
-    {
-        foreground = RASPBERRY;
-        printf(SHELL_BANNER_NONVT100);
-        foreground = LEAFGREEN;
-        printf(SHELL_START);
-        foreground = GREEN;
-    }
-    else
-#endif
-    {
-        printf(SHELL_BANNER);
-        printf(SHELL_START);
-    }
+		/* Append null character to the end of the line input */
+		buf[buflen] = '\0';
 
-    /* Continually receive and handle commands */
-    while (TRUE)
-    {
-        /* Display prompt */
-        printf(SHELL_PROMPT);
+		/* Parse line input into tokens */
+		if ( (ntok = lexan(buf, buflen, &tokbuf[0], &tok[0])) == SYSERR )
+		{
+			fprintf(descrp, SHELL_SYNTAXERR);
+			continue;
+		}
 
-        if (NULL != hostptr)
-        {
-            printf("@%s$ ", hostptr);
-        }
-        else
-        {
-            printf("$ ");
-        }
+		/* Ensure parse generated tokens */
+		if (0 == ntok) { continue; }
 
-        /* Setup proper tty modes for input and output */
-        control(stdin, TTY_CTRL_CLR_IFLAG, TTY_IRAW, NULL);
-        control(stdin, TTY_CTRL_SET_IFLAG, TTY_ECHO, NULL);
+		/* Initialize command options */
+		outname    = NULL;
+		inname     = NULL;
+		background = FALSE;
 
-        /* Read command */
-        buflen = read(stdin, buf, SHELL_BUFLEN - 1);
+		/* Mark as background process, if last token is '&' */
+		if ('&' == *tok[ntok-1])
+		{
+			ntok--;
+			background = TRUE;
+		}
 
-        /* Check for EOF and exit gracefully if seen */
-        if (EOF == buflen)
-        {
-            break;
-        }
+		/* Check each token and perform special handling of '>' and '<' */
+		for ( i = 0; i < ntok; i++ )
+		{
+			/* Background '&' should have already been handled; Syntax error */
+			if ('&' == *tok[i])
+			{
+				ntok = -1;
+				break;
+			}
 
-        /* Parse line input into tokens */
-        if (SYSERR == (ntok = lexan(buf, buflen, &tokbuf[0], &tok[0])))
-        {
-            fprintf(stderr, SHELL_SYNTAXERR);
-            continue;
-        }
+			/* Setup for output redirection if token is '>'  */
+			if ('>' == *tok[i])
+			{
+				/* Syntax error */
+				if (outname != NULL || i >= ntok-1)
+				{
+					ntok = -1;
+					break;
+				}
 
-        /* Ensure parse generated tokens */
-        if (0 == ntok)
-        {
-            continue;
-        }
+				outname = tok[i+1];
+				ntok -= 2;
 
-        /* Initialize command options */
-        inname = NULL;
-        outname = NULL;
-        background = FALSE;
+				/* shift tokens (not to be passed to command */
+				for ( j = i; j < ntok; j++)
+				{ tok[j] = tok[j+2]; }
+				continue;
+			}
 
-        /* Mark as background thread, if last token is '&' */
-        if ('&' == *tok[ntok - 1])
-        {
-            ntok--;
-            background = TRUE;
-        }
+			/* Setup for input redirection if token is '<' */
+			if ('<' == *tok[i])
+			{
+				/* Syntax error */
+				if(inname != NULL || i >= ntok-1)
+				{
+					ntok = -1;
+					break;
+				}
+				inname = tok[i+1];
+				ntok -= 2;
 
-        /* Check each token and perform special handling of '>' and '<' */
-        for (i = 0; i < ntok; i++)
-        {
-            /* Background '&' should have already been handled; Syntax error */
-            if ('&' == *tok[i])
-            {
-                ntok = -1;
-                break;
-            }
+				/* shift tokens (not to be passed to command */
+				for ( j = i; j < ntok; j++)
+				{ tok[j] = tok[j+2]; }
 
-            /* Setup for output redirection if token is '>'  */
-            if ('>' == *tok[i])
-            {
-                /* Syntax error */
-                if (outname != NULL || i >= ntok - 1)
-                {
-                    ntok = -1;
-                    break;
-                }
+				continue;
+			}
+		}
 
-                outname = tok[i + 1];
-                ntok -= 2;
+		/* Handle syntax error */
+		if (ntok <= 0)
+		{
+			fprintf(descrp, SHELL_SYNTAXERR);
+			continue;
+		}
 
-                /* shift tokens (not to be passed to command */
-                for (j = i; j < ntok; j++)
-                {
-                    tok[j] = tok[j + 2];
-                }
-                continue;
-            }
+		/* Set command devices for input, output, and error */
+		stdin  = descrp;
+		stdout = descrp;
+		stderr = descrp;
 
-            /* Setup for input redirection if token is '<' */
-            if ('<' == *tok[i])
-            {
-                /* Syntax error */
-                if (inname != NULL || i >= ntok - 1)
-                {
-                    ntok = -1;
-                    break;
-                }
-                inname = tok[i + 1];
-                ntok -= 2;
+		/* Lookup first token in the command table */
+		for (i = 0; i < ncommand; i++)
+		{
+			if (strcmp(commandtab[i].name, tok[0]) == 0) { break; }
+		}
 
-                /* shift tokens (not to be passed to command */
-                for (j = i; j < ntok; j++)
-                {
-                    tok[j] = tok[j + 2];
-                }
+		/* Handle command not found */
+		if (i >= ncommand)
+		{
+			fprintf(descrp, "%s: command not found\n", tok[0]);
+			continue;
+		}
 
-                continue;
-            }
-        }
+		/* Handle command if it is built-in */
+		if (commandtab[i].builtin)
+		{
+			if (inname != NULL || outname != NULL || background)
+			{
+				fprintf(descrp, SHELL_SYNTAXERR);
+			}
+			else
+			{
+				(*commandtab[i].procedure)(stdin, stdout, stderr, ntok, tok);
+			}
+			continue;
+		}
 
-        /* Handle syntax error */
-        if (ntok <= 0)
-        {
-            fprintf(stderr, SHELL_SYNTAXERR);
-            continue;
-        }
+		/* Spawn child process for non-built-in commands */
+		child = create(commandtab[i].procedure, SHELL_CMDSTK, SHELL_CMDPRIO, 
+		               commandtab[i].name, 5,
+		               stdin, stdout, stderr, ntok, tok);
 
-        /* Lookup first token in the command table */
-        for (i = 0; i < ncommand; i++)
-        {
-            if (0 == strcmp(commandtab[i].name, tok[0]))
-            {
-                break;
-            }
-        }
+		/* Ensure child command process was created successfully */
+		if (SYSERR == child)
+		{
+			fprintf(descrp, SHELL_CHILDERR);
+			continue;
+		} 
 
-        /* Handle command not found */
-        if (i >= ncommand)
-        {
-            fprintf(stderr, "%s: command not found\n", tok[0]);
-            continue;
-        }
+		if (background)
+		{
+			/* Make background process ready, but don't reschedule */
+			ready(child, RESCHED_NO);
+		}
+		else
+		{
+			/* Clear waiting message; Reschedule; */
+			while ( receive(FALSE) != NULL );
+			ready(child, RESCHED_YES);
 
-        /* Handle command if it is built-in */
-        if (commandtab[i].builtin)
-        {
-            if (inname != NULL || outname != NULL || background)
-            {
-                fprintf(stderr, SHELL_SYNTAXERR);
-            }
-            else
-            {
-                (*commandtab[i].procedure) (ntok, tok);
-            }
-            continue;
-        }
-
-        /* Spawn child thread for non-built-in commands */
-        child =
-            create(commandtab[i].procedure,
-                   SHELL_CMDSTK, SHELL_CMDPRIO,
-                   commandtab[i].name, 2, ntok, tok);
-
-        /* Ensure child command thread was created successfully */
-        if (SYSERR == child)
-        {
-            fprintf(stderr, SHELL_CHILDERR);
-            continue;
-        }
-
-        /* Set file descriptors for newly created thread */
-        if (NULL == inname)
-        {
-            thrtab[child].fdesc[0] = stdin;
-        }
-        else
-        {
-            thrtab[child].fdesc[0] = getdev(inname);
-        }
-        if (NULL == outname)
-        {
-            thrtab[child].fdesc[1] = stdout;
-        }
-        else
-        {
-            thrtab[child].fdesc[1] = getdev(outname);
-        }
-        thrtab[child].fdesc[2] = stderr;
-
-        if (background)
-        {
-            /* Make background thread ready, but don't reschedule */
-            im = disable();
-            ready(child, RESCHED_NO);
-            restore(im);
-        }
-        else
-        {
-            /* Clear waiting message; Reschedule; */
-            while (recvclr() != NOMSG);
-            im = disable();
-            ready(child, RESCHED_YES);
-            restore(im);
-
-            /* Wait for command thread to finish */
-            while (receive() != child);
-            sleep(10);
-        }
+			/* Wait for command process to finish */
+			while ( receive(TRUE) != child );
+			sleep(10);
+		}
     }
 
-    /* Close shell */
-    fprintf(stdout, SHELL_EXIT);
-    sleep(10);
-    return OK;
+	/* Close shell */
+	fprintf(descrp, SHELL_EXIT);
+	sleep(10);
+	return OK;
 }
